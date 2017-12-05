47e42f1c88d4abc65a2cc25b19ecb1e7001cf7a5
wuyinlei <wuyinlei19931118@hotmail.com>
2017-12-05 09:22:36 +0800

    基本配置初始化第一步

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
     Project configuration is not up-to-date with pom.xml. Select: Maven->Update Project... from the project context menu or use Quick Fix.	ocCommon		line 1	Maven Configuration Problem
 
 解决方案--->eclipse--->项目右键--->maven--->update Project  即可
+
+### 增加sql文件
---
+++ ocCommon/src/main/java/com/online/common/web/DisableUrlSessionFilter.java
@@ -0,0 +1,33 @@
+package com.online.common.web;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import java.io.IOException;
+
+public class DisableUrlSessionFilter implements Filter {
+   
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+}
